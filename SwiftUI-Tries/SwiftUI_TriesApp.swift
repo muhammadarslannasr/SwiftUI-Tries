37c91916f0cd205dbe3c37dfc3305b5f8c7e6349
Arslan <arslan.nasr@rnssol.com>
2024-01-16 16:43:01 +0500

    Initial Commit

---
+++ SwiftUI-Tries/SwiftUI_TriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TriesApp.swift
+//  SwiftUI-Tries
+//
+//  Created by Aaqib Raza on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
